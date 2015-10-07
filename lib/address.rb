d8b75f1dfcb815b70b2a9ef151a9137593f4be6e
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-10-06 23:13:21 -0700

    Attr_reader working for now.

--- lib/address.rb
+++ lib/address.rb
@@ -1,10 +1,7 @@
 class Address
-  attr_reader(:street_address, :city, :state, :zip_code)
+  attr_reader(:street_address)
   
   def initialize(attributes)
     @street_address = attributes.fetch(:street_address)
-    @city = attributes.fetch(:city)
-    @state = attributes.fetch(:state)
-    @zip_code = attributes.fetch(:zip_code)
   end
 end
